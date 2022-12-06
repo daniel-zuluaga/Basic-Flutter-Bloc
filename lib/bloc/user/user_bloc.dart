a15dc1cbb88a7b7e1370b3ce4b4bfe17c3ffdf10
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-12-05 19:40:13 -0500

    Add everything has to do with flutter bloc with all implemented in this project, with the change of state with their events

--- lib/bloc/user/user_bloc.dart
+++ lib/bloc/user/user_bloc.dart
@@ -6,9 +6,31 @@ part 'user_event.dart';
 part 'user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
-    on<UserEvent>((event, emit) {
-      // TODO: implement event handler
+  UserBloc() : super(UserInitialState()) {
+    on<ActivateUserEvent>((event, emit) => emit(UserSetState(event.user)));
+
+    on<DeleteUserEvent>((_, emit) => emit(UserInitialState()));
+
+    on<ChangeUserAgeEvent>((event, emit){
+      if(!state.existUser) return;
+      emit(UserSetState(state.user!.copyWidth(edad: event.age)));
+
+    });
+
+    on<AddProfessionEvent>((event, emit) {
+      if(!state.existUser) return;
+      final professions = [...state.user!.profesiones, event.profession];
+      emit(UserSetState(state.user!.copyWidth(profesiones: professions)));
+    });
+
+    on<ChangeUserNameEvent>((event, emit) {
+      if(!state.existUser) return;
+      emit(UserSetState(state.user!.copyWidth(nombre: event.name)));
+    });
+
+    on<ResetUserDataEvent>((event, emit) {
+      if(!state.existUser) return;
+      emit(UserSetState(event.user));
     });
   }
 }
